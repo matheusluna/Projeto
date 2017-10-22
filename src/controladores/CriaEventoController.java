dc2d50b94267fd3a5b3db0d472ded2815a6cb819
matheusluna <matheusluna96@gmail.com>
2017-10-22 21:39:11 -0200

    adicionando eventos

---
+++ src/controladores/CriaEventoController.java
@@ -0,0 +1,21 @@
+package controladores;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import interfaces.Comando;
+
+public class CriaEventoController implements Comando{
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response)
+			throws SQLException, ClassNotFoundException, IOException, ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
--- src/entidades/Local.java
+++ src/entidades/Local.java
@@ -1,5 +1,22 @@
 package entidades;
 
-public class Local {
+import java.time.LocalTime;
 
+public class Local extends Publicacao{
+	private String tipo;
+	
+	public Local(int id, String nome, String descricao, String rua, String cidade, String estado, String tipo) {
+		super(id, nome, descricao, rua, cidade, estado);
+		this.tipo = tipo;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	
+	
 }
